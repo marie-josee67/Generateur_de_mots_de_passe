16b270b4081e220e8ca9b34d737bf0eeeb49c740
marie-josee67 <mariejosee89@hotmail.fr>
2023-09-28 09:44:44 +0200

    modification avec séparation des majuscules et des minuscules pour plus de choix.

--- script.js
+++ script.js
@@ -65,12 +65,18 @@ function generatePassword(){
     let length = document.getElementById("length").value; // On à créer une variable qui s'appel : let lenght qui va contenir la valeur de notre Id lenght ( le champ ==>longueur du mot de passe).
 
     // définir les caractères à inclure dans le mot de passe.
-    let characters = "azertyuiopqsdfghjklmwxcvbnAZERTYUIOPQSDFGHJKLMWXCVBN" // Order du clavier et non alphabétique. Ils sont maintenant inclue dans la variable que l'on à créer : let characters.
+    let characters = "azertyuiopqsdfghjklmwxcvbn" // Order du clavier et non alphabétique. Ils sont maintenant inclue dans la variable que l'on à créer : let characters.
+
+    // Création d'une condition pour s'avoir si le bouton est ou non cocher.
+    if(document.getElementById("include-majuscule").checked){
+        characters += "AZERTYUIOPQSDFGHJKLMWXCVBN";
+        // += est une posibiliter d'ajout, si on avait mis que = alors l'alphabet serrait éffacer pour une nouvelle valeur siot ici les chiffres de 0 à 9.
+    }
 
     // Création d'une condition pour s'avoir si le bouton est ou non cocher.
     if(document.getElementById("include-numbers").checked){
-        characters += "0123456789";
-        // += est une posibiliter d'ajout, si on avait mis que = alors l'alphabet serrait éffacer pour une nouvelle valeur siot ici les chiffres de 0 à 9.
+      characters += "0123456789";
+      // += est une posibiliter d'ajout, si on avait mis que = alors l'alphabet serrait éffacer pour une nouvelle valeur siot ici les chiffres de 0 à 9.
     }
 
     // Création d'une condition pour les symboles.
